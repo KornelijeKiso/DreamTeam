0a02f8aa6beebec00e316f4f0040a0a39d9d5733
KornelijeKiso <kornelijekiso@gmail.com>
2023-03-10 18:47:37 +0100

    dodat Model i FileHandler za Gosta 1

---
+++ ProjectTourism/ProjectTourism/FileHandler/Guest1FileHandler.cs
@@ -0,0 +1,34 @@
+﻿using ProjectTourism.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTourism.FileHandler
+{
+    public class Guest1FileHandler
+    {
+        private Serializer<Guest1> Serializer;
+        private readonly string Filename = "../../../References/owners.csv";
+        private List<Guest1> Guests1;
+
+        public Guest1FileHandler()
+        {
+            Serializer = new Serializer<Guest1>();
+            Guests1 = Serializer.fromCSV(Filename);
+        }
+
+        public List<Guest1> Load()
+        {
+            Guests1 = Serializer.fromCSV(Filename);
+            return Guests1;
+        }
+
+        public void Save(List<Guest1> guests1)
+        {
+            Serializer.toCSV(Filename, guests1);
+        }
+    }
+
+}
