7052efc5a928291da49e2a09cf30461af4d5994d
jelly0107 <jelisaveta.letic@gmail.com>
2023-03-11 14:10:29 +0100

    Adding Guest2

---
+++ ProjectTourism/ProjectTourism/Controller/Guest2Controller.cs
@@ -0,0 +1,48 @@
+﻿using ProjectTourism.Model;
+using ProjectTourism.ModelDAO;
+using ProjectTourism.Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTourism.Controller
+{
+    public class Guest2Controller
+    {
+        private Guest2DAO Guest2DAO;
+
+        public Guest2Controller()
+        {
+            Guest2DAO = new Guest2DAO();
+        }
+
+        public void Add(Guest2 guest)
+        {
+            Guest2DAO.Add(guest);
+        }
+
+        public void Delete(Guest2 guest)
+        {
+            Guest2DAO.Delete(guest);
+        }
+
+        public List<Guest2> GetAll()
+        {
+            return Guest2DAO.GetAll();
+        }
+        public Guest2 GetOne(string username)
+        {
+            return Guest2DAO.GetOne(username);
+        }
+        public void Subscribe(IObserver observer)
+        {
+            Guest2DAO.Subscribe(observer);
+        }
+        public void NotifyObservers()
+        {
+            Guest2DAO.NotifyObservers();
+        }
+    }
+}
---
+++ ProjectTourism/ProjectTourism/Model/Guest2.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTourism.Model
+{
+    public class Guest2 : Serializable, INotifyPropertyChanged
+    {
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if (value != _username)
+                {
+                    _username = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _firstName;
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (value != _firstName)
+                {
+                    _firstName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (value != _lastName)
+                {
+                    _lastName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (value != _email)
+                {
+                    _email = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _phoneNumber;
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                if (value != _phoneNumber)
+                {
+                    _phoneNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public Guest2()
+        {
+
+        }
+
+        public Guest2(string username, string firstName, string lastName, string email, string phoneNumber)
+        {
+            this.Username = username;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+            this.PhoneNumber = phoneNumber;
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = 
+            {
+                Username,
+                FirstName,
+                LastName,
+                Email,
+                PhoneNumber     };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Username = values[0];
+            FirstName = values[1];
+            LastName = values[2];
+            Email = values[3];
+            PhoneNumber = values[4];
+        }
+    }
+}
