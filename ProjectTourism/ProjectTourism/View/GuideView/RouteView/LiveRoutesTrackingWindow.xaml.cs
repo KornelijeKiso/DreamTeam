bf629956d389f5d1ae19dac03d62c7343cf63979
Lara Petkovic <larapetkovic021@gmail.com>
2023-03-12 19:28:47 +0100

    Developing StopsView

--- ProjectTourism/ProjectTourism/View/GuideView/RouteView/LiveRoutesTrackingWindow.xaml.cs
+++ ProjectTourism/ProjectTourism/View/GuideView/RouteView/LiveRoutesTrackingWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,31 +16,58 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using ProjectTourism.Controller;
 using ProjectTourism.Model;
+using ProjectTourism.Observer;
 
 namespace ProjectTourism.View.GuideView.RouteView
 {
     /// <summary>
     /// Interaction logic for LiveRoutesTrackingWindow.xaml
     /// </summary>
-    public partial class LiveRoutesTrackingWindow : Window
+    public partial class LiveRoutesTrackingWindow : Window, INotifyPropertyChanged, IObserver
     {
         public Guide Guide { get; set; }
         public ObservableCollection<Route> Routes { get; set; }
         public Route SelectedRoute { get; set; }
         public GuideController GuideController { get; set; }
+        public RouteController RouteController { get; set; }
         public LiveRoutesTrackingWindow(string username)
         {
             InitializeComponent();
             DataContext = this;
             GuideController = new GuideController();
+            RouteController = new RouteController();
             Guide = GuideController.GetOne(username);
             Routes = new ObservableCollection<Route>(GuideController.GetGuidesRoutes(username));
         }
         public event PropertyChangedEventHandler? PropertyChanged;
-
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         public void Update()
         {
             throw new NotImplementedException();
+        }
+
+        private void StartRouteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedRoute != null)
+            {
+                //UpdateStudent updateStudent = new UpdateStudent(_controller, SelectedStudent);
+                //updateStudent.Show();
+                //_controller = updateStudent.Controller();
+                //SelectedStudent = updateStudent.getStudent();
+                //_controller.Update(SelectedStudent);
+                //UpdateStudentsList();
+                RouteStopsWindow routeStopsWindow = new RouteStopsWindow();
+                routeStopsWindow.ShowDialog();
+               
+
+            }
+            else
+            {
+                MessageBox.Show("You must choose a route which you want to start.");
+            }
         }
     }
 }
---
+++ ProjectTourism/ProjectTourism/View/GuideView/RouteView/RouteStopsWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ProjectTourism.Controller;
+using ProjectTourism.Observer;
+
+namespace ProjectTourism.View.GuideView.RouteView
+{
+    /// <summary>
+    /// Interaction logic for RouteStopsWindow.xaml
+    /// </summary>
+    public partial class RouteStopsWindow : Window, INotifyPropertyChanged, IObserver
+    {
+        public RouteController RouteController { get; set; }
+        public RouteStopsWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            RouteController = new RouteController();
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Update()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
