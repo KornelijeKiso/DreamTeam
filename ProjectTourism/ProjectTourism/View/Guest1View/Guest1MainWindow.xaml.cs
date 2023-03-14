5114ba8b0737f2623828476a12d559e87d27ead2
KornelijeKiso <kornelijekiso@gmail.com>
2023-03-15 00:19:44 +0100

    View and Search cmopleted and functional

--- ProjectTourism/ProjectTourism/View/Guest1View/Guest1MainWindow.xaml.cs
+++ ProjectTourism/ProjectTourism/View/Guest1View/Guest1MainWindow.xaml.cs
@@ -147,21 +147,21 @@ namespace ProjectTourism.View.Guest1View
 
                     string Search = LocationQuery.ToLower().Trim();
                     string[] Query = Search.ToLower().Split(',');
-                    int i = 0;
-                    foreach (string query in Query)
-                    {
-                        string currentString;
-                        currentString = query.Trim();
-                        if (currentString == "")
+                        int i = 0;
+                        foreach (string query in Query)
                         {
+                            string currentString;
+                            currentString = query.Trim();
+                            if (currentString == "")
+                            {
 
+                            }
+                            else
+                            {
+                                Query[i] = currentString;
+                                i++;
+                            }
                         }
-                        else
-                        {
-                            Query[i] = currentString;
-                            i++;
-                        }
-                    }
                     string country = accommodation.Location.Country;
                     string city = accommodation.Location.City;
                     country = country.ToLower();
@@ -169,10 +169,8 @@ namespace ProjectTourism.View.Guest1View
                     if (Query.Length == 1 && (country.Contains(Search) || city.Contains(Search)))
                     {
                         return true;
-                        
                     }
                     else if (Query.Length == 2 && (city.Contains(Query[0]) && country.Contains(Query[1])))
-
                     {
                         return true;
                     }
@@ -194,15 +192,19 @@ namespace ProjectTourism.View.Guest1View
 
         private bool TypeMatch (Accommodation accommodation)
         {
-            if (ComboType.SelectedIndex == 0 && accommodation.Type == ACCOMMODATIONTYPE.APARTMENT)
+            if (ComboType.SelectedIndex == 0)
             {
                 return true;
             }
-            else if (ComboType.SelectedIndex == 1 && accommodation.Type == ACCOMMODATIONTYPE.HOUSE)
+            if (ComboType.SelectedIndex == 1 && accommodation.Type == ACCOMMODATIONTYPE.APARTMENT)
+            {
+                    return true;
+            }
+            else if (ComboType.SelectedIndex == 2 && accommodation.Type == ACCOMMODATIONTYPE.HOUSE)
             {
                 return true;
             }
-            else if (ComboType.SelectedIndex == 2 && accommodation.Type == ACCOMMODATIONTYPE.HUT)
+            else if (ComboType.SelectedIndex == 3 && accommodation.Type == ACCOMMODATIONTYPE.HUT)
             {
                 return true;
             }
@@ -216,8 +218,6 @@ namespace ProjectTourism.View.Guest1View
         {
             FilteredAccommodations.Clear();
 
-            
-
             foreach (Accommodation accommodation in Accommodations)
             {
                 if(ReservationDaysMatch(ReservationDaysSearch, accommodation) 
@@ -229,127 +229,6 @@ namespace ProjectTourism.View.Guest1View
                     FilteredAccommodations.Add(accommodation);
                 }
             }
-
-            /*if (GuestCountSearch != "")
-            {
-                int search = int.Parse(GuestCountSearch);
-
-                foreach (Accommodation accommodation in Accommodations)
-                {
-                    int maxGuestCount = accommodation.MaxNumberOfGuests;
-
-                    if (search <= maxGuestCount)
-                    {
-                        FilteredAccommodations.Add(accommodation);
-                    }
-                }
-            }
-            else
-            {
-                foreach (Accommodation accommodation in Accommodations)
-                {
-                    FilteredAccommodations.Add(accommodation);
-                }
-            }
-
-
-
-            if (LocationSearch != "")
-            {
-
-                string Search = LocationSearch.ToLower().Trim();
-                string[] Query = Search.ToLower().Split(',');
-                int i = 0;
-                foreach (string query in Query)
-                {
-                    string currentString;
-                    currentString = query.Trim();
-                    if (currentString == "")
-                    {
-
-                    }
-                    else
-                    {
-                        Query[i] = currentString;
-                        i++;
-                    }
-                }
-
-                foreach (Accommodation accommodation in Accommodations)
-                {
-                    string country = accommodation.Location.Country;
-                    string city = accommodation.Location.City;
-                    country = country.ToLower();
-                    city = city.ToLower();
-                    if (Query.Length == 1)
-                    {
-                        if (accommodation.Location.Country.Equals(Search, StringComparison.OrdinalIgnoreCase)
-                            || accommodation.Location.City.Equals(Search, StringComparison.OrdinalIgnoreCase)
-                            || country.Contains(Search) || city.Contains(Search))
-                        {
-                            FilteredAccommodations.Add(accommodation);
-                        }
-                    }
-                    else if (Query.Length == 2)
-                    {
-                        if (city.Contains(Query[0]) && country.Contains(Query[1]))
-                        {
-                            FilteredAccommodations.Add(accommodation);
-                        }
-                    }
-                    else
-                    {
-                        FilteredAccommodations.Clear();
-                    }
-                }
-            }
-            else
-            {
-                foreach (Accommodation accommodation in Accommodations)
-                {
-                    FilteredAccommodations.Add(accommodation);
-                }
-            }
-
-
-            if(NameSearch != "")
-            {
-                string search = NameSearch.ToLower().Trim();
-                
-                foreach (Accommodation accommodation in Accommodations)
-                {
-                    string name = accommodation.Name;
-                    name = name.ToLower();
-                    
-                    if (accommodation.Name.Equals(search, StringComparison.OrdinalIgnoreCase) || name.Contains(search))
-                    {
-                        FilteredAccommodations.Add(accommodation);
-                    }  
-                }
-            }
-            else
-            {
-                foreach (Accommodation accommodation in Accommodations)
-                {
-                    FilteredAccommodations.Add(accommodation);
-                }
-            }
-
-            foreach (Accommodation accommodation in Accommodations)
-            {
-                if (ComboType.SelectedIndex == 0 && accommodation.Type == ACCOMMODATIONTYPE.APARTMENT)
-                {   
-                    FilteredAccommodations.Add(accommodation);
-                }
-                else if (ComboType.SelectedIndex == 1 && accommodation.Type == ACCOMMODATIONTYPE.HOUSE)
-                {               
-                    FilteredAccommodations.Add(accommodation);
-                }
-                else if (accommodation.Type == ACCOMMODATIONTYPE.HUT)
-                {                   
-                    FilteredAccommodations.Add(accommodation);
-                }
-            }*/
         }
         public void Update()
         {
