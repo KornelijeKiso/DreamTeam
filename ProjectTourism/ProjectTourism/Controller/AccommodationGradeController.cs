e9902329116ea089cb188c8900e5d5df28afeea2
KornelijeKiso <kornelijekiso@gmail.com>
2023-03-22 18:30:45 +0100

    Accommodation grading concept implemented

---
+++ ProjectTourism/ProjectTourism/Controller/AccommodationGradeController.cs
@@ -0,0 +1,44 @@
+﻿using ProjectTourism.Model;
+using ProjectTourism.ModelDAO;
+using ProjectTourism.Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTourism.Controller
+{
+    internal class AccommodationGradeController
+    {
+        private AccommodationGradeDAO AccommodationGradeDAO;
+        public AccommodationGradeController()
+        {
+            AccommodationGradeDAO = new AccommodationGradeDAO();
+        }
+        public void Add(AccommodationGrade accommodationGrade)
+        {
+            AccommodationGradeDAO.Add(accommodationGrade);
+        }
+        public void Delete(AccommodationGrade accommodationGrade)
+        {
+            AccommodationGradeDAO.Delete(accommodationGrade);
+        }
+        public AccommodationGrade GetOneByReservation(int reservationId)
+        {
+            return AccommodationGradeDAO.GetOneByReservation(reservationId);
+        }
+        public List<AccommodationGrade> GetAll()
+        {
+            return AccommodationGradeDAO.GetAll();
+        }
+        public void Subscribe(IObserver observer)
+        {
+            AccommodationGradeDAO.Subscribe(observer);
+        }
+        public void NotifyObservers()
+        {
+            AccommodationGradeDAO.NotifyObservers();
+        }
+    }
+}
