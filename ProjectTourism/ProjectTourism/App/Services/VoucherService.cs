969c25152f12c9b662009f3e37ba74c16faf314e
jelly0107 <jelisaveta.letic@gmail.com>
2023-04-05 18:11:53 +0200

    Adding Voucher class, refactoring

---
+++ ProjectTourism/ProjectTourism/App/Services/VoucherService.cs
@@ -0,0 +1,84 @@
+﻿using ProjectTourism.Model;
+using ProjectTourism.ModelDAO;
+using ProjectTourism.Observer;
+using ProjectTourism.Repositories;
+using ProjectTourism.Domain.IRepositories;
+using ProjectTourism.WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTourism.Services
+{
+    public class VoucherService
+    {
+        public List<IObserver> Observers;
+        private IVoucherRepository VoucherRepository;
+
+        public VoucherService(IVoucherRepository ivr)
+        {
+            Observers = new List<IObserver>();
+            VoucherRepository = ivr;
+        }
+
+        public VoucherVM GetOne(int id)
+        {
+            return new VoucherVM(VoucherRepository.GetOne(id));
+        }
+
+        public List<VoucherVM> GetAll()
+        {
+            List<VoucherVM> vouchers = new List<VoucherVM>();
+            foreach (var voucher in VoucherRepository.GetAll())
+            {
+                vouchers.Add(new VoucherVM(voucher));
+            }
+            return vouchers;
+        }
+        
+        public void Add(VoucherVM voucher)
+        {
+            VoucherRepository.Add(voucher.GetVoucher());
+        }
+
+        public void Delete(VoucherVM voucher)
+        {
+            VoucherRepository.Delete(voucher.GetVoucher());
+        }
+
+        public void Update(VoucherVM voucher)
+        {
+            VoucherRepository.Update(voucher.GetVoucher());
+        }
+
+        public List<VoucherVM> GetByGuest2(string guest2username)
+        {
+            List <VoucherVM> guest2vouchers = new List<VoucherVM> ();
+            foreach (var voucher in VoucherRepository.GetByGuest2(guest2username))
+            {
+                guest2vouchers.Add(new VoucherVM(voucher));
+            }
+            return guest2vouchers;
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            Observers.Add(observer);
+        }
+
+        public void Unsubscribe(IObserver observer)
+        {
+            Observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in Observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+}
---
+++ ProjectTourism/ProjectTourism/WPF/ViewModel/VoucherVM.cs
@@ -0,0 +1,128 @@
+﻿using ProjectTourism.Model;
+using ProjectTourism.Observer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTourism.WPF.ViewModel
+{
+    public class VoucherVM : INotifyPropertyChanged
+    {
+        private Voucher _voucher;
+
+        public VoucherVM(Voucher voucher)
+        {
+            _voucher = voucher;
+        }
+
+        public Voucher GetVoucher()
+        {
+            return _voucher;
+        }
+
+
+
+        public int Id 
+        { 
+            get => _voucher.Id;
+            set
+            {
+                if (value != _voucher.Id)
+                {
+                    _voucher.Id = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Guest2Username
+        {
+            get => _voucher.Guest2Username;
+            set
+            {
+                if (value != _voucher.Guest2Username)
+                {
+                    _voucher.Guest2Username = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public Guest2 Guest2
+        {
+            get => _voucher.Guest2;
+            set
+            {
+                if (value != _voucher.Guest2)
+                {
+                    _voucher.Guest2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime ValidFrom
+        {
+            get => _voucher.ValidFrom;
+            set
+            {
+                if (value != _voucher.ValidFrom)
+                {
+                    _voucher.ValidFrom = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime ValidDue
+        {
+            get => _voucher.ValidDue;
+            set
+            {
+                if (value != _voucher.ValidDue)
+                {
+                    _voucher.ValidDue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public STATUS Status
+        {
+            get => _voucher.Status;
+            set
+            {
+                if (value != _voucher.Status)
+                {
+                    _voucher.Status = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Description
+        {
+            get => _voucher.Description;
+            set
+            {
+                if (value != _voucher.Description)
+                {
+                    _voucher.Description = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
