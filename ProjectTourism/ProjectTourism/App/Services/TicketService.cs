d7d5bdb40437c68f001c1365c7963dc7cf4bb5bc
jelly0107 <jelisaveta.letic@gmail.com>
2023-04-05 16:04:20 +0200

    Refactoring Ticket class

---
+++ ProjectTourism/ProjectTourism/App/Services/TicketService.cs
@@ -0,0 +1,74 @@
+﻿using ProjectTourism.Model;
+using ProjectTourism.ModelDAO;
+using ProjectTourism.Observer;
+using ProjectTourism.Repositories;
+using ProjectTourism.Domain.IRepositories;
+using ProjectTourism.WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTourism.Services
+{
+    public class TicketService
+    {
+        private ITicketRepository TicketRepository;
+        public List<IObserver> Observers;
+
+        public TicketService(ITicketRepository itr)
+        {
+            TicketRepository = itr; 
+            Observers = new List<IObserver>();
+        }
+
+        public TicketVM GetOne(int Id) 
+        {
+            return new TicketVM(TicketRepository.GetOne(Id));
+        }
+
+        public List<TicketVM> GetAll() 
+        {
+            List<TicketVM> tickets = new List<TicketVM>();
+            foreach (var ticket in TicketRepository.GetAll())
+            {
+                tickets.Add(new TicketVM(ticket));
+            }
+            return tickets;
+        }
+
+        public void Add(TicketVM ticket) 
+        {
+            TicketRepository.Add(ticket.GetTicket());
+        }
+
+        public void Delete(TicketVM ticket) 
+        {
+            TicketRepository.Delete(ticket.GetTicket());
+        }
+
+        public void Update(TicketVM ticket) 
+        {
+            TicketRepository.Update(ticket.GetTicket());
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            Observers.Add(observer);
+        }
+
+        public void Unsubscribe(IObserver observer)
+        {
+            Observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in Observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+}
--- ProjectTourism/ProjectTourism/Domain/Model/Ticket.cs
+++ ProjectTourism/ProjectTourism/Domain/Model/Ticket.cs
@@ -1,8 +1,6 @@
-﻿using ProjectTourism.ModelDAO;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -11,147 +9,18 @@ using System.Windows.Media;
 
 namespace ProjectTourism.Model
 {
-    public class Ticket: Serializable, INotifyPropertyChanged
+    public class Ticket: Serializable
     {
-        private int _Id;
-        public int Id
-        {
-            get => _Id;
-            set
-            {
-                if (value != _Id)
-                {
-                    _Id = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-        
-        private SolidColorBrush _ButtonColor;
-        public SolidColorBrush ButtonColor
-        {
-            get => _ButtonColor;
-            set
-            {
-                if (value != _ButtonColor)
-                {
-                    _ButtonColor = value;
-                    OnPropertyChanged();
-                }
-            }
-        }               
-
-        private int _TourAppointmentId;
-        public int TourAppointmentId
-        {
-            get => _TourAppointmentId;
-            set
-            {
-                if (value != _TourAppointmentId)
-                {
-                    _TourAppointmentId = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private TourAppointment _TourAppointment;
-        public TourAppointment TourAppointment
-        {
-            get => _TourAppointment;
-            set
-            {
-                if (value != _TourAppointment)
-                {
-                    _TourAppointment = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private string _TourStop;
-        public string TourStop
-        {
-            get => _TourStop;
-            set
-            {
-                if (value != _TourStop)
-                {
-                    _TourStop = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private string _Guest2Username;
-        public string Guest2Username
-        {
-            get => _Guest2Username;
-            set
-            {
-                if (value != _Guest2Username)
-                {
-                    _Guest2Username = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private Guest2 _Guest2;
-        public Guest2 Guest2
-
-        {
-            get => _Guest2;
-            set
-            {
-                if (value != _Guest2)
-                {
-                    _Guest2 = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private int _numberOfGuests;
-        public int NumberOfGuests
-        {
-            get => _numberOfGuests;
-            set
-            {
-                if (value != _numberOfGuests)
-                {
-                    _numberOfGuests = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private bool _HasGuideChecked;
-        public bool HasGuideChecked
-        {
-            get => _HasGuideChecked;
-            set
-            {
-                if (_HasGuideChecked != value)
-                {
-                    _HasGuideChecked = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-        private bool _HasGuestConfirmed;
-        public bool HasGuestConfirmed
-        {
-            get => _HasGuestConfirmed;
-            set
-            {
-                if (_HasGuestConfirmed != value)
-                {
-                    _HasGuestConfirmed = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+        public int Id;
+        public SolidColorBrush ButtonColor;
+        public int TourAppointmentId;
+        public TourAppointment TourAppointment;
+        public string TourStop;
+        public string Guest2Username;
+        public Guest2 Guest2;
+        public int NumberOfGuests;
+        public bool HasGuideChecked;
+        public bool HasGuestConfirmed;
         public Ticket()
         {  }
 
@@ -159,10 +28,8 @@ namespace ProjectTourism.Model
         {
             Id = id;
             TourAppointmentId = tourAppId;
-            TourAppointment = FindTourAppointment(tourAppId);
             TourStop = tourStop;
             Guest2Username = guest2Username;
-            Guest2 = FindGuest2(guest2Username);
             NumberOfGuests = numberOfGuests;
             HasGuideChecked = false;
             HasGuestConfirmed = false;
@@ -171,31 +38,11 @@ namespace ProjectTourism.Model
         public Ticket(int tourAppId, string tourStop, string guest2Username, int numberOfGuests)
         {
             TourAppointmentId = tourAppId;
-            TourAppointment = FindTourAppointment(tourAppId);
             TourStop = tourStop;
             Guest2Username = guest2Username;
-            Guest2 = FindGuest2(guest2Username);
             NumberOfGuests = numberOfGuests;
             HasGuideChecked = false;
             HasGuestConfirmed = false;
-        }
-
-        public Guest2 FindGuest2(string username)
-        {
-            Guest2DAO guest2DAO = new Guest2DAO();
-            return guest2DAO.GetOne(username);
-        }
-        public TourAppointment FindTourAppointment(int id)
-        {
-            TourAppointmentDAO tourAppointmentDAO = new TourAppointmentDAO();
-            return tourAppointmentDAO.GetOne(id);
-        }
-        private void AddTicketToAppointment(Ticket ticket)  // possible problem
-        {
-            ticket.TourAppointment.Tickets.Add(ticket);
-            if (ticket.TourAppointment.CurrentTourStop.Equals(ticket.TourStop))
-                ticket.HasGuideChecked = true;
-            else ticket.HasGuideChecked = false; 
         }
 
         public string[] ToCSV()
@@ -217,7 +64,6 @@ namespace ProjectTourism.Model
         {
             Id = int.Parse(values[0]);
             TourAppointmentId = int.Parse(values[1]);
-            TourAppointment = FindTourAppointment(TourAppointmentId);
             Guest2Username = values[2];
             NumberOfGuests = int.Parse(values[3]);
             TourStop = values[4];
@@ -228,16 +74,7 @@ namespace ProjectTourism.Model
                 ButtonColor = Brushes.Green;
             else if (HasGuideChecked)
                 ButtonColor = Brushes.IndianRed;
-            Guest2 = FindGuest2(Guest2Username);
-            //AddTicketToAppointment(this);
-        }
-
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+           
         }
     }
 }
