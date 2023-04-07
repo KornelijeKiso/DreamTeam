17020d751e625c5d1e7009111a9a785868fa9a5b
Dušan <duci.sudjic@gmail.com>
2023-04-07 16:30:53 +0200

    BoolToVisibilityConverter finished, ready to use

---
+++ ProjectTourism/ProjectTourism/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ProjectTourism.Converters
+{
+    public class BoolToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isVisible = (bool)value;
+            return isVisible ? (object)Visibility.Visible : (object)Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility visibility = (Visibility)value;
+            return (visibility == Visibility.Visible);
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
