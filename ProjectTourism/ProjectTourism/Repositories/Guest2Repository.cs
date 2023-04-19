9664453128be43f0a10509a83299e1f9257f134a
jelly0107 <jelisaveta.letic@gmail.com>
2023-04-19 13:37:19 +0200

    Refactoring Repositories

--- ProjectTourism/ProjectTourism/Repositories/Guest2Repository.cs
+++ ProjectTourism/ProjectTourism/Repositories/Guest2Repository.cs
@@ -1,8 +1,6 @@
 ﻿using ProjectTourism.Domain.IRepositories;
 using ProjectTourism.FileHandler;
 using ProjectTourism.Model;
-using ProjectTourism.ModelDAO;
-using ProjectTourism.Observer;
 using ProjectTourism.Repositories.IRepositories;
 using System;
 using System.Collections.Generic;
