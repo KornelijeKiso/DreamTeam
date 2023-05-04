8aacadac4f05cb055c8428e1f19cb83fa41298e0
jelly0107 <jelisaveta.letic@gmail.com>
2023-05-04 17:51:49 +0200

    Adding CreateTourRequestWindow

---
+++ ProjectTourism/ProjectTourism/WPF/View/Guest2View/TicketView/CreateTourRequestWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ProjectTourism.Model;
+using ProjectTourism.WPF.ViewModel;
+using System.Globalization;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ProjectTourism.WPF.View.Guest2View.TicketView
+{
+    /// <summary>
+    /// Interaction logic for CreateTourRequestWindow.xaml
+    /// </summary>
+    public partial class CreateTourRequestWindow : Window, INotifyPropertyChanged
+    {
+        public Guest2VM Guest2 { get; set; }
+        public List<string> LanguageList { get; set; }
+        public RequestVM TourRequest { get; set; }
+           
+
+        public CreateTourRequestWindow(Guest2VM guest2)
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            Guest2 = guest2;
+            TourRequest = new RequestVM();
+
+            LanguageList = new List<string> { "English", "Spanish", "French", "German", "Italian", "Portuguese", "Russian", "Japanese", "Korean", "Chinese", "Dutch", "Swedish", "Norwegian", "Danish", "Finnish", "Turkish", "Greek", "Polish", "Arabic", "Hebrew", };
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateTourRequest(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
