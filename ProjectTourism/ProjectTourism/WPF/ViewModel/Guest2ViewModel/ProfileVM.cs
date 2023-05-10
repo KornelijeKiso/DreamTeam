0ce449f406565295622805fb9c85bb536709f732
jelly0107 <jelisaveta.letic@gmail.com>
2023-05-10 10:43:28 +0200

    Adding Profile view, small fixes in other views

---
+++ ProjectTourism/ProjectTourism/WPF/ViewModel/Guest2ViewModel/ProfileVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectTourism.Services;
+using ProjectTourism.WPF.View.Guest2View.TicketView;
+using ProjectTourism.Utilities;
+
+namespace ProjectTourism.WPF.ViewModel.Guest2ViewModel
+{
+    public class ProfileVM : ViewModelBase
+    {
+        public CurrentUserService CurrentUserService { get; set; }
+        public UserVM CurrentUser { get; set; }
+        public Guest2VM Guest2 { get; set; }
+
+        public ProfileVM()
+        {
+            SetGuest2();
+        }
+
+        public void SetGuest2()
+        {
+            CurrentUserService = new CurrentUserService();
+            CurrentUser = new UserVM(CurrentUserService.GetUser());
+            Guest2 = new Guest2VM(CurrentUser.Username);
+        }
+
+    }
+}
