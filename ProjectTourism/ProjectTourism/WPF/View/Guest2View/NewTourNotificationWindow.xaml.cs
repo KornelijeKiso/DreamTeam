e84bacb0ba8001d1c0bd8d7f34bd369c63f144c3
jelly0107 <jelisaveta.letic@gmail.com>
2023-05-18 01:38:07 +0200

    Adding NewTourNotificationWindow

---
+++ ProjectTourism/ProjectTourism/WPF/View/Guest2View/NewTourNotificationWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectTourism.WPF.View.Guest2View
+{
+    /// <summary>
+    /// Interaction logic for NewTourNotificationWindow.xaml
+    /// </summary>
+    public partial class NewTourNotificationWindow : Window
+    {
+        public NewTourNotificationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
