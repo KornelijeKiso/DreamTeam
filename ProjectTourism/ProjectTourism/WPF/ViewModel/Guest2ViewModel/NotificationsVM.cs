eb81aaaeb344a1148ad702f30625154e27a8b438
jelly0107 <jelisaveta.letic@gmail.com>
2023-05-19 17:35:36 +0200

    View details button fixed

--- ProjectTourism/ProjectTourism/WPF/ViewModel/Guest2ViewModel/NotificationsVM.cs
+++ ProjectTourism/ProjectTourism/WPF/ViewModel/Guest2ViewModel/NotificationsVM.cs
@@ -17,9 +17,12 @@ namespace ProjectTourism.WPF.ViewModel.Guest2ViewModel
         public Guest2VM Guest2 { get; set; }
         public NotificationDTO SelectedNotification { get; set; }
         public NotificationService NotificationService { get; set; }
+        public int NotificationType { get; set; } 
         public TourService TourService { get; set; }
         public TourDTO NewTour { get; set; }
         public TourVM SelectedTour { get; set; } // TO DO -> remove
+        public TicketVM ticketVM { get; set; }  // TO DO -> replace with DTO
+
         public NotificationsVM() { }
         
         public NotificationsVM(Guest2VM guest2) 
@@ -27,17 +30,8 @@ namespace ProjectTourism.WPF.ViewModel.Guest2ViewModel
             Guest2 = guest2;
 
             NotificationService = new NotificationService();
-            TourService = new TourService();    
+            TourService = new TourService();
         }
-
-        private int GetNewTourId(string text)
-        {
-            int from = text.IndexOf(':') + 1;
-            int to = text.Length - 1;
-            string idStr = text.Substring(from, to - from);
-            return int.Parse(idStr);
-        }
-
         public void SeenNotification()
         {
             List<Notification> notifications = NotificationService.GetAllByOwner(Guest2.Username);
@@ -50,26 +44,61 @@ namespace ProjectTourism.WPF.ViewModel.Guest2ViewModel
             NotificationService.Dismiss(SelectedNotification.GetNotification());
             Guest2.Notifications.Remove(SelectedNotification);
         }
-
-        // TO DO -> replace SelectedTour(TourVM) with NewTour(TourDTO) in CreateTicketWindow
         
-        //public ICommand DetailsDisplayCommand
-        //{
-        //    get => new RelayCommand(DetailsDisplayClick);
-        //}
-        //public void DetailsDisplayClick(object parameter)
-        //{
-        //    NewTour = new TourDTO(TourService.GetOne(GetNewTourId(SelectedNotification.Title)));
-        //    SelectedTour = new TourVM(TourService.GetOne(GetNewTourId(SelectedNotification.Title)));
-        //    CreateTicketWindow createTicketWidnow = new CreateTicketWindow(Guest2, SelectedTour);
-        //    createTicketWidnow.ShowDialog();
-        //}
+        private void GetNotificationType()
+        {
+            if (SelectedNotification.Title.StartsWith("New Tour")) NotificationType = 0;
+            if (SelectedNotification.Title.StartsWith("Ticket")) NotificationType = 1;
+        }
         public void DetailsDisplayClick()
         {
-            NewTour = new TourDTO(TourService.GetOne(GetNewTourId(SelectedNotification.Title)));
-            SelectedTour = new TourVM(TourService.GetOne(GetNewTourId(SelectedNotification.Title)));
-            CreateTicketWindow createTicketWidnow = new CreateTicketWindow(Guest2, SelectedTour);
-            createTicketWidnow.ShowDialog();
+            GetNotificationType();
+            switch (NotificationType)
+            {
+                // New Tour case
+                case 0:
+                    {   // TO DO -> replace SelectedTour(TourVM) with NewTour(TourDTO) in CreateTicketWindow
+                        NewTour = new TourDTO(TourService.GetOne(GetNewTourId(SelectedNotification.Title)));
+                        SelectedTour = new TourVM(TourService.GetOne(GetNewTourId(SelectedNotification.Title)));
+                        CreateTicketWindow createTicketWidnow = new CreateTicketWindow(Guest2, SelectedTour);
+                        createTicketWidnow.ShowDialog();
+                        break;
+                    }
+                // Tour Attendance case
+                case 1:
+                    {
+                        ticketVM = Guest2.Tickets.First(r => r.Id == GetTicketId(SelectedNotification.Title));
+                        Guest2AttendanceWindow guest2AttendanceWindow = new Guest2AttendanceWindow(ticketVM, Guest2);
+                        guest2AttendanceWindow.ShowDialog();
+                        
+                        // TO DO -> check if ticket.TourAppointment EXPIRED
+                        //if (ticketVM.TourAppointment.State == TOURSTATE.STARTED)
+                        //{ }
+                        //else
+                        //{ }
+                        break;
+                    }
+                // error case
+                default:
+                    {
+                        
+                        break;
+                    }
+            }
+        }
+        private int GetNewTourId(string text)
+        {
+            int from = text.IndexOf(':') + 1;
+            int to = text.Length - 1;
+            string idStr = text.Substring(from, to - from);
+            return int.Parse(idStr);
+        }
+        private int GetTicketId(string text)
+        {
+            int from = text.IndexOf(':') + 1;
+            int to = text.Length - 1;
+            string idStr = text.Substring(from, to - from);
+            return int.Parse(idStr);
         }
     }
 }
