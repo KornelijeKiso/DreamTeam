993933edd0174c0f6991cb1c982563059549e124
Lara Petkovic <larapetkovic021@gmail.com>
2023-05-29 13:04:31 +0200

    Complex tour requests view

--- ProjectTourism/ProjectTourism/WPF/View/GuideView/RequestsView/ComplexToursUserControl.xaml.cs
+++ ProjectTourism/ProjectTourism/WPF/View/GuideView/RequestsView/ComplexToursUserControl.xaml.cs
@@ -1,32 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ProjectTourism.DTO;
+using ProjectTourism.Model;
 
 namespace ProjectTourism.WPF.View.GuideView.RequestsView
 {
-    /// <summary>
-    /// Interaction logic for ComplexToursUserControl.xaml
-    /// </summary>
     public partial class ComplexToursUserControl : UserControl
     {
         public GuideDTO Guide { get; set; }
+        public ObservableCollection<ComplexTourDTO> ComplexTours { get; set; }
+        public ComplexTourDTO SelectedComplexTour { get; set; }
         public ComplexToursUserControl(String username)
         {
             InitializeComponent();
             DataContext = this;
             Guide = new GuideDTO(username);
+            ComplexTours = Guide.ComplexTours;
         }
     }
 }
