da267b16a765adb8e2b5ec26ea9502bf0d5064d6
Lara Petkovic <larapetkovic021@gmail.com>
2023-05-31 23:13:40 +0200

    pdf report start

---
+++ ProjectTourism/ProjectTourism/PDF/GuidePDF/GuestsOnATourPDFGenerator.cs
@@ -0,0 +1,30 @@
+﻿using ProjectTourism.DTO;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Draw;
+using iText.Layout;
+using iText.Layout.Properties;
+using iText.Layout.Element;
+using iText.Kernel.Colors;
+
+namespace ProjectTourism.PDF.GuidePDF
+{
+    public class GuestsOnATourPDFGenerator
+    {
+        private PdfWriter pdfWriter { get; set; }
+        private PdfDocument pdfDocument { get; set; }
+        private Document document { get; set; }
+
+        public GuestsOnATourPDFGenerator(TourAppointmentDTO tourApp)
+        {
+            pdfWriter = new PdfWriter("../../../PDF/GuidePDF/tourAppointmentReposrt_" + tourApp.Id.ToString() + ".pdf");
+            pdfDocument = new PdfDocument(pdfWriter);
+            document = new Document(pdfDocument);
+
+            Paragraph info = new Paragraph("Report for " + tourApp.Tour.Name)
+                .SetTextAlignment(TextAlignment.CENTER)
+                .SetFontSize(20);
+            document.Add(info);
+
+        }
+    }
+}
