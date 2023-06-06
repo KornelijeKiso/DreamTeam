d1d35832722e9fd72e18cb473456b4e751ac0dc8
Lara Petkovic <larapetkovic021@gmail.com>
2023-06-06 10:13:56 +0200

    Viewer of pdfs finished

---
+++ ProjectTourism/ProjectTourism/WPF/View/GuideView/PDFViewer.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Syncfusion.Windows.PdfViewer;
+
+namespace ProjectTourism.WPF.View.GuideView
+{
+    public partial class PDFViewer : Window
+    {
+        public PDFViewer(string path)
+        {
+            InitializeComponent();
+            pdfViewerControl.Load(path);
+        }
+    }
+}
