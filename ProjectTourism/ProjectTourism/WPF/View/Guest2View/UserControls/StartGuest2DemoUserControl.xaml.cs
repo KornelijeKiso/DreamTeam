43cebeeaf668fe6ee8110652b1c5b20223767703
jelly0107 <jelisaveta.letic@gmail.com>
2023-06-07 08:17:54 +0200

    Adding popup for the start of Demo

---
+++ ProjectTourism/ProjectTourism/WPF/View/Guest2View/UserControls/StartGuest2DemoUserControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ProjectTourism.WPF.ViewModel.Guest2ViewModel;
+
+namespace ProjectTourism.WPF.View.Guest2View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for StartGuest2DemoUserControl.xaml
+    /// </summary>
+    public partial class StartGuest2DemoUserControl : UserControl
+    {
+        StartGuest2DemoVM currentVM;
+        public StartGuest2DemoUserControl()
+        {
+            InitializeComponent();
+            currentVM = new StartGuest2DemoVM(this);
+            this.DataContext = currentVM;
+        }
+    }
+}
---
+++ ProjectTourism/ProjectTourism/WPF/ViewModel/Guest2ViewModel/StartGuest2DemoVM.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using ProjectTourism.Utilities;
+
+namespace ProjectTourism.WPF.ViewModel.Guest2ViewModel
+{
+    public class StartGuest2DemoVM
+    {
+        public bool On { get; set; }
+        public UserControl PopUp { get; set; }
+
+        public ICommand GoCommand { get; set; }
+        private void GoAction(object obj) 
+        {
+            On = true;
+            PopUp.Visibility = System.Windows.Visibility.Hidden;
+        }
+        public ICommand CancelCommand { get; set; }
+        private void CancelAction(object obj)
+        {
+            On = false;
+            PopUp.Visibility = System.Windows.Visibility.Hidden;
+        }
+        
+        public StartGuest2DemoVM(UserControl control)
+        {
+            this.On = false;
+            this.PopUp = control;
+            GoCommand = new RelayCommand(GoAction);
+            CancelCommand = new RelayCommand(CancelAction);
+        }
+    }
+}
