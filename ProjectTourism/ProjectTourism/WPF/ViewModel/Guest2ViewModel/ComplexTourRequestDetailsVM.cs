7c4c0b07e14b5ad0a3b4a536a1db249e4ad34a86
jelly0107 <jelisaveta.letic@gmail.com>
2023-06-09 16:23:23 +0200

    Adding ComplexTourRequestDetails - UserControl and VM

---
+++ ProjectTourism/ProjectTourism/WPF/ViewModel/Guest2ViewModel/ComplexTourRequestDetailsVM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ProjectTourism.DTO;
+using ProjectTourism.Utilities;
+
+
+namespace ProjectTourism.WPF.ViewModel.Guest2ViewModel
+{
+    public class ComplexTourRequestDetailsVM : ViewModelBase
+    {
+        public Guest2DTO Guest2 { get; set; }
+        public ComplexTourDTO ComplexTour { get; set; }
+        public DateTime ValidUntil { get; set; }
+        public TourRequestDTO SelectedPart { get; set; }
+
+        private bool _isAccepted;
+        public bool isAccepted
+        {
+            get => _isAccepted; 
+            set 
+            {
+                if (value != _isAccepted)
+                {
+                    _isAccepted = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        private object _Content;
+        public object Content
+        {
+            get { return _Content; }
+            set { _Content = value; OnPropertyChanged(); }
+        }
+        public ComplexTourRequestDetailsVM() { }
+        public ComplexTourRequestDetailsVM(Guest2DTO guest2, ComplexTourDTO complexTour) 
+        {
+            Guest2 = guest2;
+            ComplexTour = complexTour;
+
+            ValidUntil = ComplexTour.TourRequests[0].CreationDateTime.AddDays(2);
+
+            // Commands
+            ContentCommand = new RelayCommand(ReturnToComplexTours);
+        }
+
+
+
+        public ICommand ContentCommand { get; set; }
+        public void ReturnToComplexTours(object obj)
+        {
+            Content = new ComplexToursVM(Guest2);
+        }
+    }
+}
